bcdc1136a2cde4d33897e018b3d1834f9f1c6837
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-03-11 15:54:36 -0300

    feat (api) adicionar classe de configuration

---
+++ src/main/java/io/github/cursospringjpa/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,54 @@
+package io.github.cursospringjpa.libraryapi.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    } //nao é recomendado usar esse.é basic
+
+    @Bean
+    public DataSource hikariDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setMaximumPoolSize(10); //max de conexao liberada
+        config.setMinimumIdle(1); //tamanho inicial do pool
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(60000); //600 mil ms(1O MINUTOS)
+        config.setConnectionTimeout(10000); //timeout para validar conexao
+        config.setConnectionTestQuery("select 1"); //query de teste
+
+        return new HikariDataSource(config);
+
+
+    }
+}
