6b8b326125316e5400b3c05aeb884d171018165c
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-03-20 17:08:58 -0300

    Revert "feature add dependencias pom"
    This reverts commit b4eb07447de592b6417607c0781616511ed13abf.

--- src/main/java/io/github/cursospringjpa/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/cursospringjpa/libraryapi/controller/dto/AutorDTO.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
