138123864c1a545a8cdd210aef30c4348e9f709b
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-03-30 16:01:43 -0300

    feat(exception) implementa exception personalizada

--- src/main/java/io/github/cursospringjpa/libraryapi/exceptions/OperacaoNaoPermitidaException.java
+++ src/main/java/io/github/cursospringjpa/libraryapi/exceptions/OperacaoNaoPermitidaException.java
@@ -1,7 +1,14 @@
 package io.github.cursospringjpa.libraryapi.exceptions;
+<<<<<<< HEAD
 
+=======
+>>>>>>> 5f686c0 (feat(exception) implementa exception personalizada)
 public class OperacaoNaoPermitidaException extends RuntimeException {
     public OperacaoNaoPermitidaException(String message) {
         super(message);
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 5f686c0 (feat(exception) implementa exception personalizada)
