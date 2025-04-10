756408ff4fb38d1e5419c5c5a82fdc66c20caa14
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-04-10 19:43:28 -0300

    feature (controller) adicionar endpoints

--- src/main/java/io/github/cursospringjpa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursospringjpa/libraryapi/controller/AutorController.java
@@ -3,6 +3,7 @@ package io.github.cursospringjpa.libraryapi.controller;
 import io.github.cursospringjpa.libraryapi.controller.dto.AutorDTO;
 import io.github.cursospringjpa.libraryapi.controller.dto.AutorResponseDTO;
 import io.github.cursospringjpa.libraryapi.controller.dto.ErroResponse;
+import io.github.cursospringjpa.libraryapi.controller.mappers.AutorMapper;
 import io.github.cursospringjpa.libraryapi.exceptions.OperacaoNaoPermitidaException;
 import io.github.cursospringjpa.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.cursospringjpa.libraryapi.model.Autor;
@@ -11,6 +12,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -19,112 +21,81 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("autores")
-public class AutorController {
+public class AutorController implements GenericController {
 
     private final AutorService service;
+    private final AutorMapper mapper;
 
-    public AutorController(AutorService service) {
+    public AutorController(AutorService service, AutorMapper mapper) {
         this.service = service;
+        this.mapper = mapper;
     }
 
     @PostMapping
-    public ResponseEntity<Object> salvar(@Valid @RequestBody AutorDTO autor){
-        try {
-            Autor autorEntidade = autor.mapearParaAutor();
-            service.salvar(autorEntidade);
+    public ResponseEntity<Void> salvar(@Valid @RequestBody AutorDTO dto) {
 
-            //http://localhost:8080/autores/{id}
-            URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                    .path("/{id}")
-                    .buildAndExpand(autorEntidade.getId())
-                    .toUri();
-
-
-            return ResponseEntity.created(location).build();
-        }catch (RegistroDuplicadoException e){
-            var erroDto = ErroResponse.conflito(e.getMessage());
-            return ResponseEntity.status(erroDto.status()).body(erroDto);
-        }
+        Autor autor = mapper.toEntity(dto);
+        service.salvar(autor);
+        var url = gerarHeaderLocation(autor.getId());
+        return ResponseEntity.created(url).build();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<AutorResponseDTO> obterDetalhes(@PathVariable("id") String id){
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
         var idAutor = UUID.fromString(id);
-        Optional<Autor> autorOptional = service.obterPorId(idAutor);
-        if (autorOptional.isPresent()){
-            Autor autor = autorOptional.get();
-            AutorResponseDTO autorResponseDTO = new AutorResponseDTO(
-                    autor.getId(),
-                    autor.getNome(),
-                    autor.getDataNascimento(), autor.getNacionalidade());
-            return ResponseEntity.ok(autorResponseDTO);
 
-        }
-        return ResponseEntity.notFound().build();
+        return service.obterPorId(idAutor).map(autor -> {
+            AutorDTO dto = mapper.toDTO(autor);
+            return ResponseEntity.ok(dto);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Object> deletarAutor(@PathVariable("id") String id){
-        try {
-            var idAutor = UUID.fromString(id);
-            Optional<Autor> autorOptional = service.obterPorId(idAutor);
+    public ResponseEntity<Void> deletarAutor(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
 
-            if (autorOptional.isEmpty()) {
-                return ResponseEntity.notFound().build();
-            }
-
-            service.deletarAutor(autorOptional.get());
-
-            return ResponseEntity.noContent().build();
-        }catch (OperacaoNaoPermitidaException e){
-            var erroResponse = ErroResponse.respostaPadrao(e.getMessage());
-            return ResponseEntity.status(erroResponse.status()).body(erroResponse);
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
+
+        service.deletarAutor(autorOptional.get());
+
+        return ResponseEntity.noContent().build();
+
     }
 
     @GetMapping
-    public ResponseEntity<List<AutorResponseDTO>> pesquisar(
-            //param nao obrigatorios
-            @RequestParam(value ="nome", required=false) String nome,
-            @RequestParam(value = "nacionalidade", required=false) String nacionalidade){
-        List<Autor> resultado = service.pesquisaByExample(nome,nacionalidade);
-        List<AutorResponseDTO> lista = resultado
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+        List<Autor> resultado = service.pesquisaByExample(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
                 .stream()
-                .map(autor -> new AutorResponseDTO(
-                        autor.getId(),
-                        autor.getNome(),
-                        autor.getDataNascimento(),
-                        autor.getNacionalidade())
-                ).collect(Collectors.toList());
-        //retorna a lista
-                return ResponseEntity.ok(lista);
+                .map(mapper::toDTO)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Object> atualizar(
-            @PathVariable("id") String id, @RequestBody AutorResponseDTO autorResponseDTO) {
+    public ResponseEntity<Void> atualizar(@PathVariable("id") String id, @RequestBody AutorResponseDTO autorResponseDTO) {
 
-        try {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
 
-            var idAutor = UUID.fromString(id);
-            Optional<Autor> autorOptional = service.obterPorId(idAutor);
-
-            if (autorOptional.isEmpty()) {
-                return ResponseEntity.notFound().build();
-            }
-
-            var autor = autorOptional.get();
-            autor.setNome(autorResponseDTO.nome());
-            autor.setNacionalidade(autorResponseDTO.nacionalidade());
-            autor.setDataNascimento(autorResponseDTO.dataNascimento());
-
-            service.atualizar(autor);
-
-            return ResponseEntity.noContent().build();
-
-        } catch(RegistroDuplicadoException e){
-            var erroDto = ErroResponse.conflito(e.getMessage());
-            return ResponseEntity.status(erroDto.status()).body(erroDto);
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
+
+        var autor = autorOptional.get();
+        autor.setNome(autorResponseDTO.nome());
+        autor.setNacionalidade(autorResponseDTO.nacionalidade());
+        autor.setDataNascimento(autorResponseDTO.dataNascimento());
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+
     }
 }
--- src/main/java/io/github/cursospringjpa/libraryapi/controller/GenericController.java
+++ src/main/java/io/github/cursospringjpa/libraryapi/controller/GenericController.java
@@ -5,13 +5,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.UUID;
 
-//public interface GenericController {
-//
-//    default URI gerarHeaderLocation(UUID id){
-//        return ServletUriComponentsBuilder
-//                .fromCurrentRequest()
-//                .path("/{id}")
-//                .buildAndExpand(id)
-//                .toUri();
-//    }
-//}
+public interface GenericController {
+
+    default URI gerarHeaderLocation(UUID id){
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
