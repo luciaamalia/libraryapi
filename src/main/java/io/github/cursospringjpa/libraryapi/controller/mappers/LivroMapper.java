d2a2c274012267c8264474299d6bd147922b08ad
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-04-10 19:45:20 -0300

    feature (mapper) adicionar mapper

--- src/main/java/io/github/cursospringjpa/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/cursospringjpa/libraryapi/controller/mappers/LivroMapper.java
@@ -8,19 +8,15 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 @Mapper(componentModel = "spring", uses = AutorMapper.class )
 public abstract class LivroMapper {
 
     @Autowired
     AutorRepository autorRepository;
 
-    public LivroMapper(AutorRepository autorRepository) {
-        this.autorRepository = autorRepository;
-    }
-
-    @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
+    @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null)  )")
     public abstract Livro toEntity(RequestLivroDTO dto);
 
     public abstract ResponsePesquisaDTO toDTO(Livro livro);
+
 }
--- src/main/java/io/github/cursospringjpa/libraryapi/mapper/AutorMapper.java
+++
@@ -1,8 +0,0 @@
-package io.github.cursospringjpa.libraryapi.mapper;
-
-import ch.qos.logback.core.model.ComponentModel;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface AutorMapper {
-}
