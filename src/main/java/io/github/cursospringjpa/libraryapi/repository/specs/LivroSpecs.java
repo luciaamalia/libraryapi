e99127b6d7b5f4a3957cecefcbea4d5a5126c4f1
Lúcia Amália <lucia.amaliaof@gmail.com>
2025-04-10 19:45:34 -0300

    feature (specs) adicionar specs

--- src/main/java/io/github/cursospringjpa/libraryapi/repository/specs/LivroSpecs.java
+++ src/main/java/io/github/cursospringjpa/libraryapi/repository/specs/LivroSpecs.java
@@ -8,33 +8,40 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class LivroSpecs {
 
-    public static Specification<Livro> isbnEqual(String isbn){
-        return (root, query, cb) -> cb.equal(root.get("isbn"), isbn);
+    //retorna se o isbn é igual ao parametro de comparacao
+    public static Specification<Livro> isbnEqual( String isbn){
+        return (root, query,  cb) -> cb.equal(root.get("isbn"), isbn);
     }
 
-    public static Specification<Livro> tituloLike(String titulo){
-        // upper(livro.titulo) like (%:param%)
+    public static Specification<Livro> tituloLike (String titulo){
+        //
         return (root, query, cb) ->
-                cb.like( cb.upper(root.get("titulo")), "%" + titulo.toUpperCase() + "%");
+        cb.like( cb.upper(root.get("titulo")),"%" + titulo.toUpperCase() + "%");
     }
 
     public static Specification<Livro> generoEqual(GeneroLivro genero){
-        return (root, query, cb) -> cb.equal(root.get("genero"), genero);
+        return (root, query, cb) -> cb.equal(root.get("genero"),genero);
+
+
     }
 
     public static Specification<Livro> anoPublicacaoEqual(Integer anoPublicacao){
-        // and to_char(data_publicacao, 'YYYY') = :anoPublicacao
-        return (root, query, cb) ->
-                cb.equal( cb.function("to_char", String.class,
-                        root.get("dataPublicacao"), cb.literal("YYYY")),anoPublicacao.toString());
-    }
+        //to_char (data_publicacao, 'DD/MM/YYYY') = :anoPublicacao
+        return (root, query, cb) -> cb.equal(
+                cb.function("to_char", String.class,
+                        root.get("dataPublicacao"), cb.literal("YYYY")) ,anoPublicacao.toString());
 
-    public static Specification<Livro> nomeAutorLike(String nome){
+
+    }   public static Specification<Livro> nomeAutorLike(String nome){
+        //to_char (data_publicacao, 'DD/MM/YYYY') = :anoPublicacao
         return (root, query, cb) -> {
-            Join<Object, Object> joinAutor = root.join("autor", JoinType.INNER);
-            return cb.like( cb.upper(joinAutor.get("nome")), "%" + nome.toUpperCase() + "%" );
+//
+            Join<Object, Object> joinAutor = root.join("autor", JoinType.LEFT);
+            return  cb.like(cb.upper(joinAutor.get("nome")), "%" + nome.toUpperCase() + "%");
 
-//            return cb.like( cb.upper(root.get("autor").get("nome")), "%" + nome.toUpperCase() + "%" );
+//           return cb.like( cb.upper(root.get("autor").get("nome")), "%" + nome.toUpperCase() + "%");
         };
     }
+
+
 }
